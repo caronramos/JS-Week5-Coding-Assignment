d735caecb20f4f33eb871ab87941284f85dcae46
Caron Ramos <98505172+caronramos@users.noreply.github.com>
2022-03-05 15:49:24 -0800

    Update JSWeek5CodingAssignment.js

--- JSWeek5CodingAssignment.js
+++ JSWeek5CodingAssignment.js
@@ -1,1 +1,40 @@
+class WorshipPracticeAttendance {
+  constructor(player, instrument) {
+    this.player = player;
+    this.instrument = instrument;
+  }
+  
+  describe() {
+    return ${this.player} plays ${this.instrument}.;
+    
+  }
+}
 
+class WorshipTeam {
+  constructor(player) {
+    this.player = player;
+    this.names = [];
+  }
+}
+
+addName(name) {
+  if (name instanceof Name) {
+    this.names.push(name);
+  } else {
+    throw new Error('You can only add an instance of Name. Argument is not a worship player: ${name}');
+  }
+  
+  describe() {
+    return ${this.player} has ${this.names.length} players.;
+  }
+}
+
+class Menu {
+  constructor() {
+    this.teams = [];
+    this.selectedTeam = null;
+  }
+  
+  start() {
+    let selection = this.showMainMenuOptions();
+  }
